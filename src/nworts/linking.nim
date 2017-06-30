be46fd6720999f8e10c0751ac0d5c3744c2d051a
Charles Barto <bartoc@umich.edu>
2017-06-30 19:43:24 -0400

    more work on linking

--- src/nworts/linking.nim
+++ src/nworts/linking.nim
@@ -3,4 +3,10 @@ import logging
 
 proc link_directory*(src, dst: string) =
     for path in walkDirRec(src):
+        var relpath = path[src.len..^1]
+        if symlinkExists(src / relpath):
+            error("Symlink exists")
+            raise newException(IOError, "Symlink did not exist")
+        createSymlink(path, src / relpath)
         
+            
